d60076f1ef26f25c04351d4f0c162d1b01f37f4e
kishorebabudasari <86375860+kishorebabudasari@users.noreply.github.com>
2025-05-31 00:16:30 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // ✅ Changed here
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 import Home from "./components/Home";
@@ -8,7 +8,7 @@ import Teaching from "./components/Teaching";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import './App.css'
+import './App.css';
 import Publications from "./components/Publications";
 import Talks from "./components/Talks";
 import Students from "./components/Students";
@@ -19,25 +19,23 @@ import Skills from "./components/Skills";
 
 function App() {
   return (
-    <Router>
+    <Router> {/* ✅ Now using HashRouter */}
       <div className="main-panel">
+        <Navbar />
         <div>
-          <Navbar />
-          <div className="">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/publications" element={<Publications />} />
-              <Route path="/students" element={<Students />} />
-              <Route path="/activities" element={<Activities />} />
-              <Route path="/experience" element={<Experience />} />
-              <Route path="/skills" element={<Skills />} />
-              <Route path="/certificates" element={<Certificates />} />
-              <Route path="/talks" element={<Talks />} />
-              <Route path="/teaching" element={<Teaching />} />              
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/publications" element={<Publications />} />
+            <Route path="/students" element={<Students />} />
+            <Route path="/activities" element={<Activities />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="/certificates" element={<Certificates />} />
+            <Route path="/talks" element={<Talks />} />
+            <Route path="/teaching" element={<Teaching />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </div>
         <Footer />
       </div>
