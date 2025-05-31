007998b0a7524cdb825c1a61c94b79c1f63dea43
UNDRATHI RAVITEJA <155756850+undrathi9133@users.noreply.github.com>
2025-05-31 17:26:36 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
-import { StrictMode } from 'react';
+// main.jsx
+import { StrictMode } from 'react'; 
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
-);
+const root = document.getElementById('root');
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
