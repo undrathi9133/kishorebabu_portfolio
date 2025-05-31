aa71262dc57135261081a8b755099a9b4ac99e79
UNDRATHI RAVITEJA <155756850+undrathi9133@users.noreply.github.com>
2025-05-31 17:16:30 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kishorebabu_portfolio/'  // Add this line
+  base: '/kishorebabudasari.com/'  // Add this line
 })
