acd7916216df7e00f3f4961d089038720169d991
UNDRATHI RAVITEJA <155756850+undrathi9133@users.noreply.github.com>
2025-05-31 12:30:37 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';  // ✅ Add this line
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>   {/* ✅ Wrap App in HashRouter */}
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
