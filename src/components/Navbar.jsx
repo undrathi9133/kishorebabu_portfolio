5743f73d9dcd01a443016c8d3a134fce68c0370c
UNDRATHI RAVITEJA <155756850+undrathi9133@users.noreply.github.com>
2025-05-31 12:35:17 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import collegeLogo from '../assets/kmit_logo.jpg';
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="/"><img src={collegeLogo} /></a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        {/* Use Link instead of <a> for SPA routing */}
+        <Link className="navbar-brand" to="/">
+          <img src={collegeLogo} alt="College Logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-            <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
-            <li className="nav-item"><a className="nav-link" href="/publications">Publications</a></li>
-            <li className="nav-item"><a className="nav-link" href="/students">Students</a></li>
-            <li className="nav-item"><a className="nav-link" href="/activities">Activities</a></li>
-            <li className="nav-item"><a className="nav-link" href="/experience">Experience</a></li>
-            <li className="nav-item"><a className="nav-link" href="/skills">Skills</a></li>
-            <li className="nav-item"><a className="nav-link" href="/certificates">Certificates</a></li>
-            <li className="nav-item"><a className="nav-link" href="/talks">Talks</a></li>
-            <li className="nav-item"><a className="nav-link" href="/teaching">Teaching</a></li>            
-            <li className="nav-item"><a className="nav-link" href="/contact">Contact</a></li>
+            <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/publications">Publications</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/students">Students</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/activities">Activities</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/experience">Experience</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/skills">Skills</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/certificates">Certificates</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/talks">Talks</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/teaching">Teaching</Link></li>
+            <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
